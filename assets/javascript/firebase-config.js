ae47d182365fc718bf304f1e358fa7fccc6aeb54
Eugio <47667122+ejhalpin@users.noreply.github.com>
2019-06-23 14:28:14 -0400

    user authorization schema complete. Users can now create an account, sign in / out of an account, and link guest accounts to permanent ones

---
+++ assets/javascript/firebase-config.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCr7ZdjRuOSChN3BIdNNa2l3SEtgQiHl2Y",
+  authDomain: "recipe-box-d4385.firebaseapp.com",
+  databaseURL: "https://recipe-box-d4385.firebaseio.com",
+  projectId: "recipe-box-d4385",
+  storageBucket: "",
+  messagingSenderId: "813897633460",
+  appId: "1:813897633460:web:789fb9b6748cf3e2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Iconfinder Api secure-information
+var clientID = "RZRl4YS26HBHgzCo0mq8qZkAoUxNzSXs8knSVxqGLnGE65gAJUV9VbbNZThEhyDC";
+var clientSecret = "x7VI5XOmv9JmFgGXB6rAwLhqSVutaSPfk5otLovpQjSfyWsYhI2VAnEFzlWtsO5n";
+var apiEndPoint = "https://api.iconfinder.com/v3/icons/search?query=";
