42d4f32ec6065587d24ceeadcdcd207d37ce37ec
frostyasian <zphang@gmail.com>
2019-06-25 18:59:38 -0400

    config

---
+++ assets/javascript/firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDfQdEV3qyvJiCn8DDAau9stCrPERFbVLQ",
+  authDomain: "project1-bab30.firebaseapp.com",
+  databaseURL: "https://project1-bab30.firebaseio.com",
+  projectId: "project1-bab30",
+  storageBucket: "",
+  messagingSenderId: "686045408466",
+  appId: "1:686045408466:web:b65d6661e432870d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
